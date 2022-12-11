3ba3b662d20507eb9588558fb907082547cbd426
Josh Mogil <joshmogil@pop-os.localdomain>
2022-12-11 18:28:20 -0500

    pcis

--- fb_new.py
+++ fb_new.py
@@ -1,5 +1,11 @@
 import pyautogui
 import time
+import dotenv , os
+
+dotenv.load_dotenv(".env")
+
+mob = os.getenv("FB_MOBILE")
+pas = os.getenv("FB_PASSWORD")
 
 delay = 3
 screenWidth, screenHeight = pyautogui.size()
@@ -15,7 +21,18 @@ double_click = pyautogui.doubleClick
 #3. facebook mobilized
 #----> see vobjects screenshots to see exactly what it is looking for
 click(pyautogui.locateCenterOnScreen("./vobjects/firefox.png",confidence=.75))
-double_click(pyautogui.locateCenterOnScreen("./vobjects/full_screen_browser.png",confidence=.75))
+#double_click(pyautogui.locateCenterOnScreen("./vobjects/full_screen_browser.png",confidence=.75))
+time.sleep(.30)
+click(pyautogui.locateCenterOnScreen("./vobjects/facebook_shortcut.png",confidence=.75))
+time.sleep(3)
+
+click(pyautogui.locateCenterOnScreen("./vobjects/fblogin/mobile.png",confidence=.75))
+time.sleep(.30)
+pyautogui.typewrite(mob)
+
+click(pyautogui.locateCenterOnScreen("./vobjects/fblogin/pw_box.png",confidence=.75))
+time.sleep(.30)
+pyautogui.typewrite(pas)
 
 
 print(screenWidth, screenHeight)
