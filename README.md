8b5b31d568c995c22adc2ce5ad77873004954dc7
Josh Mogil <joshmogil@pop-os.localdomain>
2022-12-11 16:38:37 -0500

    read me, pyauto

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # amiro-9000
+To run on linux you will need apt install the following packages:
+
+sudo apt-get install scrot
+sudo apt-get install python3-tk python3-dev
--- fb_new.py
+++ fb_new.py
@@ -1,4 +1,8 @@
 import pyautogui
 
+delay = 3
 screenWidth, screenHeight = pyautogui.size()
+#pyautogui.moveTo(0, 0, delay)
+firefox = pyautogui.locateCenterOnScreen("./vobjects/firefox.png",confidence=.75)
+pyautogui.leftClick(firefox)
 print(screenWidth, screenHeight)
