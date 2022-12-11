3827fe709373ef9322ebdd88de1d54b4fcc1d6df
Josh Mogil <joshmogil@pop-os.localdomain>
2022-12-11 16:48:43 -0500

    vobjects

--- fb_new.py
+++ fb_new.py
@@ -1,8 +1,21 @@
 import pyautogui
+import time
 
 delay = 3
 screenWidth, screenHeight = pyautogui.size()
 #pyautogui.moveTo(0, 0, delay)
-firefox = pyautogui.locateCenterOnScreen("./vobjects/firefox.png",confidence=.75)
-pyautogui.leftClick(firefox)
+
+
+click = pyautogui.leftClick
+double_click = pyautogui.doubleClick
+
+####some assumptions###
+#1. pop os linux 
+#2. using mozilla firefox
+#3. facebook mobilized
+#----> see vobjects screenshots to see exactly what it is looking for
+click(pyautogui.locateCenterOnScreen("./vobjects/firefox.png",confidence=.75))
+double_click(pyautogui.locateCenterOnScreen("./vobjects/full_screen_browser.png",confidence=.75))
+
+
 print(screenWidth, screenHeight)
