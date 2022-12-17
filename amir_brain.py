d4edc599c97b2006d24935a19c165fb555612317
Josh Mogil <jmogi@LAPTOP-4MBDPAMO.localdomain>
2022-12-16 22:30:55 -0500

    vignesh

--- amir_brain.py
+++ amir_brain.py
@@ -1,22 +1,131 @@
 from datetime import datetime
 import holidays
+import time
+from chat_wrap import talk
+import random
 
-# Select country
 us_holidays = holidays.US()
 today_str = datetime.today().strftime('%m-%d-%Y')
-now = datetime.datetime.now()
+now = datetime.today()
+
+day_enu={
+    1:"monday",
+    2:"tuesday",
+    3:"wednesday",
+    4:"thursday",
+    5:"friday",
+    6:"saturday",
+    7:"sunday"
+}
+
+def week_enu(week_int):
+    if week_int <= 11 or week_int > 47:
+        return "winter"
+    if week_int <= 23:
+        return "spring"
+    if week_int <= 35:
+        return "summer"
+    if week_int <= 47:
+        return "fall"
+
+def get_athlete():
+    return random.choice([
+        "Lebron James",
+        "Michael Jordan",
+        "Steph Curry",
+        "Jalen Hurts",
+        "Kevin Durant",
+        "The Shah",
+        "Bruce Jenner"
+        ])
+
+def get_enemy():
+    return random.choice([
+        "Vig",
+        "Nikhil"
+        ])
+
+def get_weekday_response(weekday):
+    if weekday == "monday":
+        return random.choice([
+            "starting the week strong and going to the gym",
+            "birds chirping, sweating hard and working out"
+            ])
+    if weekday == "tuesday":
+        return random.choice([
+            "waking up enjoying life, smell the flowers",
+            f"Call out {get_enemy()} for being foul"
+
+            ])
+    if weekday == "wednesday":
+        return random.choice([
+            f"can't let {get_enemy()} win. It's hump day",
+            "hump day, half way through the battle!",
+            f"What do you think {get_athlete()} would do if he woke up in your shoes?"
+            ])
+    if weekday == "thursday":
+        return random.choice([
+            f"aspiration achieving our dreams." ,
+            "ONE MORE DAY!",
+            f"Another nightmare about the ayatollah."
+            ])
+    if weekday == "friday":
+        return random.choice([
+            "telling close friends good job this week.",
+            "This is it! Grand finale!"
+            ])
+    if weekday == "saturday":
+        return random.choice([
+            f"fake friendliness towards {get_enemy()}",
+            "Saturday. Men are made."
+            ])
+    if weekday == "sunday":
+        return random.choice([
+            f"rest up big guys, it's about to repeat."
+            ])
+
+def determine_prompt(holiday= None, day= None, week= None):
+    if day == None:
+        day = now.isoweekday()
+    if holiday == None:
+        holiday = us_holidays.get(today_str)
+    if week == None:
+        week = now.isocalendar().week
+    season = week_enu(week)
+    day = day_enu[day]
+    #print(f"Holiday: {holiday}, Day: {day}, Season: {season}")
+    if holiday == "New Year's Day":
+        return f"Happy new years message"
+    if holiday == "Martin Luther King Jr. Day":
+        return f"gratitude for Martin Luther King Jr."
+    if holiday == "Memorial Day":
+        return f"Excitement for the start of summer, memorial day"
+    if holiday == "Independence Day":
+        return f"disdain for british colonizers. Love America!"
+    if holiday == "Labor Day":
+        return f"Praiseful retrospect about how hard everyone works"
+    if holiday == "Columbus Day":
+        return f"Hateful statement about christopher columbus"
+    if holiday == "Veterans Day":
+        return f"Admiration for veterans, go off on tangent about the how great the shah of iran was"
+    if holiday == "Christmas Day":
+        return f"enjoyment of christmas and love for the shah"
+    
+    pr = get_weekday_response(day)
+    print(pr) 
+    return pr
 
 
-print(today_str)
+if __name__ == "__main__":
+    for j in range(5):
+        for i in range (1,8):
+            
+            print(talk(determine_prompt(day = i)))
+            time.sleep(6.5)
 
-holiday = us_holidays.get('1-17-2022')
-holiday = holiday if holiday not in ["Christmas Day (Observed)", "Juneteenth National Independence Day (Observed)"] else False
-
-for ptr in holidays.US(years = int(today_str.split('-')[2])).items():
-    print(ptr)
-
-
-print(now.isoweekday())
-
-def determine_prompt(holiday,now):
-    print(now.isoweekday())
+    exit()
+    for ptr in holidays.US(years = int(today_str.split('-')[2])).items():
+        pr = determine_prompt(ptr[1])
+        if pr != None:
+            time.sleep(1.85)
+            print(talk(pr))
--- chat_wrap.py
+++ chat_wrap.py
@@ -9,18 +9,89 @@ from dotenv import load_dotenv
 load_dotenv("./.env")
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-def talk(prompt):
+def clean_tail(response):
+  try:
+    if response[-1] not in ".?!":
+      comma = response.rfind(",")
+      period = response.rfind(".")
+      mark = response.rfind("?")
+      exclaim = response.rfind("!")
+      last_ender= max([comma,period,mark,exclaim])
+      response = response[0:last_ender+1]
+      if last_ender == comma:
+        response = response[0:-1]+"."
+    if not response[0].isalpha():
+      response = response[1:]
+
+    return response
+  except:
+    return response
+
+
+def get_personality():
+    return random.choice([
+        "man"
+        ])
+
+def get_seed():
+    new_list = []
+  
+    [new_list.append("random motivational saying about life") for i in range (12)]
+    [new_list.append(f"random motivation saying about the gym") for i in range (12)]
+    [new_list.append(f"random motivation saying about the waking up early not about birds") for i in range (12)]
+    [new_list.append("statement about grinding hard at the gym") for i in range (12)]
+    [new_list.append(f"statement about waking up incredibly early") for i in range (12)]
+    [new_list.append(f"very negative statement about vignesh lacking morals") for i in range (10)]
+    [new_list.append(f"very negative statement about nikhil being nasty") for i in range (10)]
+
+    choice = random.choice(new_list)
+    print(choice)
+    return choice
+
+def seed():
   time.sleep(random.randrange(12,31)/10)
   response = openai.Completion.create(
     model="text-davinci-003",
-    prompt=prompt,
+    prompt=get_seed(),
     temperature=1,
-    max_tokens=98,
+    max_tokens=40,
     top_p=1,
-    frequency_penalty=0.32,
-    presence_penalty=0.65
+    frequency_penalty=2,
+    presence_penalty=2
   )
-  return response
+  response = response["choices"][0]["text"].replace("\n","").replace("?","")
+  print(response)
+  return clean_tail(response)
+
+
+def talk(prompt):
+
+  time.sleep(random.randrange(12,31)/10)
+  response = openai.Completion.create(
+    model="text-davinci-003",
+    prompt=f"energetic short {prompt} in the style of an excited {get_personality()}",
+    temperature=1,
+    max_tokens=40,
+    top_p=1,
+    frequency_penalty=2,
+    presence_penalty=2
+  )
+  response = clean_tail(response["choices"][0]["text"].replace("\n","").replace("?",""))
+  
+  if "Nikhil".upper() in prompt.upper() and "Nikhil".upper() not in response.upper():
+    print("triggered")
+    talk(prompt)
+  else:
+    return clean_tail(response)
+  if "Vignesh".upper() in prompt.upper() and "Vignesh".upper() not in response.upper():
+    talk(prompt)
+  else:
+    return clean_tail(response)
+
+if __name__ =="__main__":
+  
+        
+  print(talk(seed()))
 
 def respond(message, feeling):
   time.sleep(random.randrange(12,31)/10)
@@ -34,9 +105,10 @@ def respond(message, feeling):
   presence_penalty=0.65
   )
   return response
-
+"""
 for feeling in ["happy","insightful", "angry", "hateful", "stupid", "vindicative"]:
   print(f"ChatGPT responding as a {feeling} person, using prompt wraps:")
   resp = respond("Was barrack obama a good president?", feeling)
   print(resp["choices"][0]["text"].replace("\n","").replace("?",""))
   print('\n')
+"""
